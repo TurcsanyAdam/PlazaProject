520c92927d79a6c54de281b9d6de9ea31a4a3151
Turcsanyadam <turcsanyadam@gmail.com>
2020-02-10 14:07:38 +0100

    Create skeleton for Plaza interface

---
+++ PlazaProjectAPI/Plaza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlazaProjectAPI
+{
+    interface Plaza
+    {
+        private List<Shop> getShops()
+        {
+
+        }
+        private void addShop(Shop shop)
+        {
+
+        }
+        private void removeShop(Shop shop)
+        {
+
+        }
+        private Shop findShopByName(string name)
+        {
+
+        }
+        private bool isOpen()
+        {
+
+        }
+        private void open()
+        {
+
+        }
+        private void close()
+        {
+
+        }
+
+        private string ToString()
+        {
+
+        }
+    }
+}
