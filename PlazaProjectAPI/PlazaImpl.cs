a11c84f6811ecd072866087e30b017da25d34f32
Turcsanyadam <turcsanyadam@gmail.com>
2020-02-10 14:10:15 +0100

    Create skeleton for PlazaImpl class

---
+++ PlazaProjectAPI/PlazaImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlazaProjectAPI
+{
+    class PlazaImpl
+    {
+        public List<Shop> shops;
+
+        private PlazaImpl()
+        {
+
+        }
+    }
+}
